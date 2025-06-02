6d0a6a270d1c272000e192d4ebfdc915a5857e88
root@zp-nc04 <root@zp-nc04>
2025-06-02 12:23:31 +0800

    增加部署文件

---
+++ deploy_docker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "▶ 停止旧容器..."
+docker stop search_github && docker rm search_github
+
+echo "▶ 删除镜像"
+docker rmi search_github
+
+echo "▶ 构建新镜像..."
+docker build -t search_github:latest .
+
+echo "▶ 启动新容器..."
+docker run -d -p 3003:3003 --name search_github search_github:latest
+
+echo "✅ 更新完成！"
