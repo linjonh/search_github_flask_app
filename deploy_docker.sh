2ba96c4d83ce6ed5454dc181e843493bf749e079
root@zp-nc04 <root@zp-nc04>
2025-06-02 13:15:53 +0800

    修改镜像名称

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -7,9 +7,9 @@ echo "▶ 删除镜像"
 docker rmi search_github
 
 echo "▶ 构建新镜像..."
-docker build -t search_github:latest .
+docker build -t ghcr.io/linjonh/search_github:latest .
 
 echo "▶ 启动新容器..."
-docker run -d -p 3003:3003 --name search_github search_github:latest
+docker run -d -p 3003:3003 --name search_github ghcr.io/linjonh/search_github:latest
 
 echo "✅ 更新完成！"
